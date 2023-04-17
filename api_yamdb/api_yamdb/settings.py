e40d1128ac6279061f74a59c3e6ebf9ad5e6eaab
Владимир Свириденко <vova.sviridenko6gmail.com>
2023-04-17 17:59:52 +0300

    Add redoc.html)))

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -110,6 +110,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
 AUTH_USER_MODEL = 'reviews.User'
 
 REST_FRAMEWORK = {
